abdfff73d120aa2cd88722f2e6f7b88d4368dc15
Yen-Wen Chen <wenchen@Yen-WentekiMacBook-Air.local>
2012-09-11 10:51:34 +0800

    Facebook Login

---
+++ js/app.js
@@ -0,0 +1,33 @@
+fb_oauthurl = "https://www.facebook.com/dialog/oauth/";
+fb_scope = "email,user_birthday";
+fb_clientid = "286934128081818";
+fb_display = "popup";
+fb_authurl = "http://54.248.75.147/fbauth.html";
+
+fblogin = function() {
+  var FBLogin_URL, fbauth;
+  FBLogin_URL = fb_oauthurl + "?scope=" + encodeURIComponent(fb_scope) + "&redirect_uri=" + encodeURIComponent(fb_authurl) + "&display=" + encodeURIComponent(fb_display) + "&client_id=" + encodeURIComponent(fb_clientid);
+  fbauth = window.open(FBLogin_URL, 'fbwindow', 'height=200, width=450, top=300, left=300, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no');
+  fbauth.focus();
+  return this;
+};
+
+function MMLoginCtrl($scope, $http, $location) {
+  window.fbAuthResp = function(code) {
+    var data;
+    if (code === -1 || code === -2) {
+      return alert("Facebook回傳資料錯誤! code: " + code);
+    } else {
+      data = {
+        code: code,
+        redirect_uri: fb_authurl
+      };
+      $http.post('/api/Login/',$.param(data)).success(function(data) {
+        data = data.json[0].Message[1];
+        if (data.Success === true) {
+          $location.path('/main')
+        }
+      });
+    }
+  };
+}
